c9279fda2b619e4196087ac3b0e516eee1faa32a
Erik Mikac <mikacew@gmail.com>
2021-03-07 08:32:07 -0500

    Finished second practice of day 32 Animation Practice.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,32 @@
 //
 
 import SwiftUI
-
+/*
+ In this project we are binding animation to a button.
+ */
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        print(animationAmount)
+        /*
+         Since we added non-view code, we need to add return to the VStack. That way Swift know what it is returning.
+         */
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            
+            Button("Tap Me") {
+          //      self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
     }
 }
 
