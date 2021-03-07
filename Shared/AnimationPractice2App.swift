ed6bbf476ed03ac970cecbc6f96382ac3effda53
Erik Mikac <mikacew@gmail.com>
2021-03-07 08:05:12 -0500

    Initial Commit

---
+++ Shared/AnimationPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationPractice2App.swift
+//  Shared
+//
+//  Created by Erik Mikac on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
